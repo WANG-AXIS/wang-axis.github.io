2209165d8b9d5a16523cf55d176bac6dfb0ef87f
Chuang Niu <niuchuangnn@gmail.com>
2024-05-12 23:07:28 -0400

    Update login-script.js

--- js/login-script.js
+++ js/login-script.js
@@ -1,6 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const loginButton = document.querySelector('.login-button');
-    loginButton.addEventListener('click', function () {
-        alert('User not exist!');
-    });
-});
+function login() {
+    alert('User not exist!');
+}
